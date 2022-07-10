28611bad7a1fe7f37767c3edde6c8e692a8513bc
limheesun4898 <heehee4898@gmail.com>
2022-07-10 19:55:59 +0900

    fix eslint and add command delpoy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    extends: "airbnb",
-    plugins: ["react"],
-    rules: {
-        'comma-dangle': false,
-        'react/jsx-filename-extension': false,
-        'quotes': false,
-        'semi': false,
-        'max-len': false,
-        'jsx-a11y/click-events-have-key-events': false,
-        'jsx-a11y/no-static-element-interactions': false,
-        'import/no-duplicates': false,
-    },
+  extends: "airbnb",
+  plugins: ["react"],
+  rules: {
+    'comma-dangle': 'off',
+    'react/jsx-filename-extension': 'off',
+    quotes: 'off',
+    semi: 'off',
+    'max-len': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'import/no-duplicates': 'off',
+  },
 };
