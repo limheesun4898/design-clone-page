48a5b81582beed948a7d14e0ee8342b013582e03
limheesun4898 <heehee4898@gmail.com>
2022-07-10 19:48:03 +0900

    add gh-pages

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: "airbnb",
+    plugins: ["react"],
+    rules: {
+        'comma-dangle': false,
+        'react/jsx-filename-extension': false,
+        'quotes': false,
+        'semi': false,
+        'max-len': false,
+        'jsx-a11y/click-events-have-key-events': false,
+        'jsx-a11y/no-static-element-interactions': false,
+        'import/no-duplicates': false,
+    },
+};
