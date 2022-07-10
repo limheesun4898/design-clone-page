c44d00c1e73b0b825ec4096c43d53adc13ff71cd
limheesun4898 <heehee4898@gmail.com>
2022-07-10 23:26:20 +0900

    design mouse center

--- src/components/MouseOverContent.jsx
+++ src/components/MouseOverContent.jsx
@@ -4,9 +4,18 @@ import PropTypes from "prop-types";
 const MouseOverContent = function ({ clientX, clientY }) {
   return (
     <div className="mouse-wrapper">
-      <div className="x" style={{ top: `${clientY}px` }} />
-      <div className="y" style={{ left: `${clientX}px` }} />
-      <div className="center" />
+      {clientX && clientY && (
+        <>
+          <div className="x" style={{ top: `${clientY}px` }} />
+          <div className="y" style={{ left: `${clientX}px` }} />
+          <div
+            className="center"
+            style={{ top: `${clientY}px`, left: `${clientX}px` }}
+          >
+            <div />
+          </div>
+        </>
+      )}
     </div>
   );
 };
