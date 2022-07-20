b7963051bf25d8c360b7e4894a0ad0bc7f4dca67
limheesun4898 <heehee4898@gmail.com>
2022-07-20 15:56:23 +0900

    path change

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import Introduction from './pages/Introduction';
 
 function App() {
   return (
-    // <Router basename={process.env.PUBLIC_URL}>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <main>
         <Routes>
-          <Route path="/design-clone-page" element={<Introduction />} />
+          <Route path="/" element={<Introduction />} />
         </Routes>
       </main>
     </Router>
